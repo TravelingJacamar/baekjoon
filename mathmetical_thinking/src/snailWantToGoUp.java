c9cbc7dbf3dbee1fe5c661d943fe2e0b6da353eb
taejulee <ltj0319@naver.com>
2025-05-01 22:02:40 +0900

    3일차

--- mathmetical_thinking/src/snailWantToGoUp.java
+++ mathmetical_thinking/src/snailWantToGoUp.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class snailWantToGoUp {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //낮에 올라가는 미터 A 밤에 미끄러지는 미터 B 나무막대 총높이 V
+        int A = input.nextInt();
+        int B = input.nextInt();
+        int V = input.nextInt();
+
+
+        int days = (V-A+(A-B)-1)/(A-B)+1;
+
+        System.out.println(days);
+
+    }
 }
